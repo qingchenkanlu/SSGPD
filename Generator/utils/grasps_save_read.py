4d3c1f6c17b2f14d0c4f4e83568dce623df1eb94
MrRen-sdhm <rzy.1996@qq.com>
2019-12-08 15:27:28 +0800

    read and show grasps of ycb or fusion dataset

--- Generator/utils/grasps_save_read.py
+++ Generator/utils/grasps_save_read.py
@@ -36,33 +36,39 @@ def show_grasps_info(grasps):
         print("[%.1f-%.1f]: %d" % (min_score, max_score, len(m)))
 
 
-def show_grasps(obj, grasps):
+def show_grasps(obj, grasps, ply_name=None):
     m = np.array(grasps)
     m_good = m[m[:, 1] <= 0.6]  # NOTE: 摩擦系数<=0.6为质量高的抓取
     m_good = m_good[m_good[:, 1] >= 0.0]
-    m_good = m_good[np.random.choice(len(m_good), size=5, replace=True)]  # 随机选择25个显示
+    m_good = m_good[np.random.choice(len(m_good), size=10, replace=True)]  # 随机选择25个显示
     m_bad = m[m[:, 1] >= 0.7]  # NOTE: 摩擦系数>=0.7为质量低的抓取
     m_bad = m_bad[m_bad[:, 1] <= 3.0]
-    m_bad = m_bad[np.random.choice(len(m_bad), size=5, replace=True)]  # 随机选择25个显示
+    m_bad = m_bad[np.random.choice(len(m_bad), size=10, replace=True)]  # 随机选择25个显示
 
     ags = GpgGraspSampler(gripper, sample_config)
 
     mlab.figure(bgcolor=(1, 1, 1), fgcolor=(0.7, 0.7, 0.7), size=(1024, 768))
     for good in m_good:
         ags.display_grasps3d(good[0], 'g')
-    ags.show_surface_points(obj)
+    if ply_name is None:
+        ags.show_surface_points(obj)
+    else:
+        mlab.pipeline.surface(mlab.pipeline.open(ply_name))
     mlab.title("good", size=0.5)
     # mlab.show()
 
     mlab.figure(bgcolor=(1, 1, 1), fgcolor=(0.7, 0.7, 0.7), size=(1024, 768))
     for bad in m_bad:
         ags.display_grasps3d(bad[0], 'r')
-    ags.show_surface_points(obj)
+    if ply_name is None:
+        ags.show_surface_points(obj)
+    else:
+        mlab.pipeline.surface(mlab.pipeline.open(ply_name))
     mlab.title("bad", size=0.5)
     mlab.show()
 
 
-def show_grasps_range(obj, grasps, min_score=0.0, max_score=3.2):
+def show_grasps_range(obj, grasps, ply_name=None, min_score=0.0, max_score=3.2):
     m = np.array(grasps)
     m = m[m[:, 1] <= max_score]  # NOTE: 摩擦系数<=0.6为质量高的抓取
     m = m[m[:, 1] >= min_score]
@@ -75,7 +81,10 @@ def show_grasps_range(obj, grasps, min_score=0.0, max_score=3.2):
     mlab.figure(bgcolor=(1, 1, 1), fgcolor=(0.7, 0.7, 0.7), size=(1024, 768))
     for grasp in m:
         ags.display_grasps3d(grasp[0], 'g')
-    ags.show_surface_points(obj)
+    if ply_name is None:
+        ags.show_surface_points(obj)
+    else:
+        mlab.pipeline.surface(mlab.pipeline.open(ply_name))
     mlab.title("%.1f-%.1f" % (min_score, max_score), size=0.5)
     mlab.show()
 
@@ -93,30 +102,63 @@ def grasps_read(filename):
         return [grasps]
 
 
-if __name__ == '__main__':
-    file_dir = "../../Dataset/fusion/"
-    sample_config = YamlConfig("../config/sample_config.yaml")
-    gripper = RobotGripper.load("../config/gripper_params.yaml")
+def load_grasps(dataset):
+    grasps, cloud, cloud_voxel, mesh_path = None, None, None, None
+    if dataset == "fusion":
+        grasps, cloud, cloud_voxel, mesh_path = load_grasps_fusion()
+    elif dataset == "ycb":
+        grasps, cloud, cloud_voxel, mesh_path = load_grasps_ycb()
+    return grasps, cloud, cloud_voxel, mesh_path
 
+
+def load_grasps_fusion():
+    file_dir = "../../Dataset/fusion/"
     obj_name = "2018-04-24-15-57-16"
     obj_path = file_dir + obj_name
     pickle_path = obj_path + "/grasps.pickle"
+
     cloud_path = obj_path + "/processed/surface_cloud_with_normals.pcd"
+    mesh_path = obj_path + "/processed/mesh.ply"
     cloud_voxel_path = obj_path + "/processed/surface_cloud_voxel.pcd"
-
     cloud = o3d.io.read_point_cloud(cloud_path)
+    # cloud = o3d.io.read_point_cloud(cloud_voxel_path)  # Note: show voxeled cloud
     cloud_voxel = o3d.io.read_point_cloud(cloud_voxel_path)
-
-    graspable_obj = GraspableObject(cloud, cloud_voxel)
     grasps = grasps_read(pickle_path)
 
-    show_grasps(graspable_obj, grasps)
+    return grasps, cloud, cloud_voxel, mesh_path
+
+
+def load_grasps_ycb():
+    file_dir = "../../Dataset/ycb/ycb_meshes_google/"
+    obj_name = "006_mustard_bottle"
+    obj_path = file_dir + obj_name
+    pickle_path = obj_path + "/grasps.pickle"
+
+    cloud_path = mesh_path = obj_path + "/google_512k/nontextured.ply"
+    cloud_voxel_path = obj_path + "/google_512k/nontextured_voxel.ply"
+    # cloud = o3d.io.read_point_cloud(cloud_path)
+    cloud = o3d.io.read_point_cloud(cloud_voxel_path)  # Note: show voxeled cloud
+    cloud_voxel = o3d.io.read_point_cloud(cloud_voxel_path)
+    grasps = grasps_read(pickle_path)
+
+    return grasps, cloud, cloud_voxel, mesh_path
+
+
+if __name__ == '__main__':
+
+    grasps, cloud, cloud_voxel, mesh_path = load_grasps("ycb")  # ["fusion", "ycb"]
+
+    sample_config = YamlConfig("../config/sample_config.yaml")
+    gripper = RobotGripper.load("../config/gripper_params.yaml")
+
+    graspable_obj = GraspableObject(cloud, cloud_voxel)
+    show_grasps(graspable_obj, grasps, ply_name=mesh_path)
     # show_grasps_range(obj, grasps, min_score=0.0, max_score=0.5)
     show_grasps_info(grasps)
 
     min_scores = np.linspace(0.0, 3.2, num=30)
     # print(min_scores)
     for min_score in min_scores:
-        show_grasps_range(graspable_obj, grasps, min_score=min_score, max_score=min_score+0.1)
+        show_grasps_range(graspable_obj, grasps, ply_name=mesh_path, min_score=min_score, max_score=min_score+0.1)
 
 
